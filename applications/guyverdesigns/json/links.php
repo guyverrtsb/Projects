3532c8ce812a771977bfd726b6bbdbefe5e0847d
Guyver <Guyver@10.239.16.121>
2014-08-04 16:56:38 -0400

    Large File Upload

--- applications/guyverdesigns/json/links.php
+++ applications/guyverdesigns/json/links.php
@@ -5,7 +5,7 @@
     { "display":"Usersafety Index", "url":"/gd.trxn.com/usersafety/index.php" },
     { "display":"Usersafety Home", "url":"/s_user_home.php" },
     { "display":"Site Template", "url":"/index.php" },
-    { "display":"Large File Share", "url":"/utilities/largefileshare.php" },
+    { "display":"Large File Share", "url":"/utilities/largefileshare/upload.php" },
     { "display":"-----------", "url":"#" },
     { "display":"mavericks-109-image-mega-part-1", "url":"http://j.gs/1421644/mavericks-109-image-mega-part-1" },
     { "display":"mavericks-109-image-mega-part-2", "url":"http://j.gs/1421644/mavericks-109-image-mega-part-2" },
