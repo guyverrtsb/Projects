286a00de68fc31d92b9c1af9559be196fdef8a11
Guyver <Guyver@gddevlpr01>
2014-08-10 13:46:24 -0400

    Switch from GroupUniv to University Life

---
+++ applications/guyverdesigns/utilities/largefileshare/upload.js
@@ -0,0 +1,53 @@
+$(document).ready(function()
+{
+//elements
+    var progressbox     = $("#GDUploadProgressBox");
+    var progressbar     = $("#GDUploadProgressBar");
+    var statustxt       = $("#GDUploadProgressBarStatusTxt");
+    var submitbutton    = $("#GDUploadSubmitButton");
+    var myform          = $("#GDUploadImageFrm");
+    var output          = $("#TransactionErr");
+    var completed       = "0%";
+    var message         = "";
+
+    $(myform).ajaxForm({
+        data: { GD_CONTROLLER_KEY: "REGISTER_WALL_MESSAGE" },
+        beforeSend: function() { //before sending form
+            submitbutton.attr("disabled", ""); // disable upload button
+            statustxt.empty();
+            progressbox.show(); //show progress bar
+            progressbar.width(completed); //initial value 0% of progress bar
+            statustxt.html(completed); //set status text
+            statustxt.css("color","#000"); //initial color of status text
+            message = "";
+            output.text(message);
+            clearOutputResults();
+        },
+        uploadProgress: function(event, position, total, percentComplete) { //on progress
+            progressbar.width(percentComplete + "%") //update progress bar percent complete
+            statustxt.html(percentComplete + "%"); //update status text
+            if(percentComplete>50)
+                {
+                    statustxt.css("color","#fff"); //change status text to white after 50%
+                }
+            },
+        complete: function(data)
+        { // on complete
+            //output.html(response.responseText); //update element with received data
+            submitbutton.removeAttr("disabled"); //enable submit button
+            progressbox.hide(); // hide progress bar
+            data = eval('(' + data.responseText + ')')
+            if(buildContentBlocksReturnMessage(data, "SUCCESS", "GDTransactionOutput"))
+            {
+                // gdLoadNewWallMessages();
+            }
+            myform.resetForm();  // reset form
+        }
+    });
+});
+
+function clearOutputResults()
+{
+    //$("#GDUploadImage").text("");
+    //$("#GDUploadReturnMessage").text("");
+}
